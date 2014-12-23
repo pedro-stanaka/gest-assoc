031210a040a31ce11b84e045e3d6577b886c61f5
Pedro Sena Tanaka <pedro.stanaka@gmail.com>
2014-12-23 19:26:36 -0200

    Created payment types.

---
+++ config/Migrations/20141223210816_create_payment_types.php
@@ -0,0 +1,26 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreatePaymentTypes extends AbstractMigration
+{
+
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+        $this->table('payment_types')
+            ->addColumn('name','string')
+            ->addColumn('description', 'text')
+            ->addColumn('amount_months', 'integer');
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+        $this->dropTable('payment_types');
+    }
+}
